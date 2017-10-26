3c3560e24167102137a555ad403b697560429e05
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-26 21:20:14 +0700

    add platforms, physics

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Phaser Game with Phaser.io and TypeScript
+# Phaser Game with Phaser.io
 
 ## Links
 - [Phaser.io Tutorial](http://phaser.io/tutorials/making-your-first-phaser-game/index)
-- [Phaser.io with TypeScript](http://phaser.io/tutorials/how-to-use-phaser-with-typescript)
 
 made with :heart: by [Steven](https://github.com/iamstevendao).
--- app.js
+++ app.js
@@ -8,24 +8,61 @@ function preload () {
 }
 
 var platforms
+var player
+var cursors
 
 function create () {
     game.physics.startSystem(Phaser.Physics.ARCADE)
     game.add.sprite(0, 0, 'sky')
+
     platforms = game.add.group()
     platforms.enableBody = true
+
+    // ground
     let ground = platforms.create(0, game.world.height - 64, 'ground')
     ground.scale.setTo(2, 2)
     ground.body.immovable = true
 
+    // 2 ledges
     let ledge = platforms.create(400, 400, 'ground')
     ledge.body.immovable = true
     ledge = platforms.create(-150, 250, 'ground')
     ledge.body.immovable = true
-    game.add.sprite(0, 0, 'star')
 
+    // player
+    player = game.add.sprite(32, game.world.height - 150, 'dude')
+    game.physics.arcade.enable(player)
+
+    // physics properties
+    player.body.bounce.y = 0.2
+    player.body.gravity.y = 300
+    player.body.collideWorldBounds = true
+
+    // player animations when go left or right
+    player.animations.add('left', [0, 1, 2, 3], 10, true)
+    player.animations.add('right', [5, 6, 7, 8], 10, true)
+
+    cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update () {
+    // player collides platforms
+    let hitPlatform = game.physics.arcade.collide(player, platforms)
 
+    player.body.velocity.x = 0
+
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150
+        player.animations.play('left')
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150
+        player.animations.play('right')
+    } else {
+        player.animations.stop()
+        player.frame = 4
+    }
+
+    if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
+        player.body.velocity.y = -350
+    }
 }
