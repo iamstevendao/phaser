7f9fce2088bffd1971db871af6fd5d070ca2d53e
Steven Dao <iamstevendao@users.noreply.github.com>
2017-10-27 19:17:50 +0700

    add stars

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function preload () {
 var platforms
 var player
 var cursors
+var stars
 
 function create () {
     game.physics.startSystem(Phaser.Physics.ARCADE)
@@ -42,27 +43,46 @@ function create () {
     player.animations.add('left', [0, 1, 2, 3], 10, true)
     player.animations.add('right', [5, 6, 7, 8], 10, true)
 
+    // stars
+    stars = game.add.group()
+    stars.enableBody = true
+
+    for (let i = 0; i < 12; i++) {
+        let star = stars.create(i * 70, 0, 'star')
+        star.body.gravity.y = 6
+        star.body.bounce.y = 0.7 + Math.random() * 0.2
+    }
+
     cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update () {
     // player collides platforms
     let hitPlatform = game.physics.arcade.collide(player, platforms)
-
+    game.physics.arcade.collide(stars, platforms)
+    game.physics.arcade.overlap(player, stars, collectStar, null, this)
     player.body.velocity.x = 0
 
     if (cursors.left.isDown) {
+        // move to the left
         player.body.velocity.x = -150
         player.animations.play('left')
     } else if (cursors.right.isDown) {
+        // move to the right
         player.body.velocity.x = 150
         player.animations.play('right')
     } else {
+        // stand still
         player.animations.stop()
         player.frame = 4
     }
 
+    // jump when user press UP when touching the ground
     if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
         player.body.velocity.y = -350
     }
+}
+
+function collectStar (player, star) {
+    star.kill()
 }
